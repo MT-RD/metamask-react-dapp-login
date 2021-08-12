0be5f9f310555c2a72362f89abac54551f8132f8
Manoj Talagadadeevi <manojt.854@gmail.com>
2021-08-12 15:24:27 -0400

    completed the layout for the Account Modal

--- src/components/AccountModal.tsx
+++ src/components/AccountModal.tsx
@@ -1,15 +1,17 @@
 import {useEthers} from "@usedapp/core";
 import {
   Box, Button,
-  Flex,
+  Flex, Link,
   Modal,
   ModalBody,
   ModalCloseButton,
-  ModalContent,
+  ModalContent, ModalFooter,
   ModalHeader,
   ModalOverlay,
   Text
 } from "@chakra-ui/react";
+import Identicon from "./Identicon";
+import {CopyIcon, ExternalLinkIcon} from "@chakra-ui/icons";
 
 
 type Props = {
@@ -75,11 +77,76 @@ export default function AccountModal({ isOpen, onClose }) {
               </Button>
             </Flex>
 
+            <Flex alignItems="center" mt={2} mb={4} lineHeight={1}>
+              <Identicon />
+              <Text color="white"
+                    fontSize="xl"
+                    fontWeight="semibold"
+                    ml={2}
+                    lineHeight="1.1"
+              >
+                {account &&
+                  `${account.slice(0, 6)}...${account.slice(
+                    account.length - 4,
+                    account.length
+                  )}`
+                }
+              </Text>
+            </Flex>
+
+            <Flex alignContent="center" m={3}>
+              <Button
+                variant="link"
+                color="gray.400"
+                fontWeight="normal"
+                fontSize="sm"
+                _hover={{
+                  textDecoration: "none",
+                  color: "whiteAlpha.800",
+                }}
+              >
+                <CopyIcon mr={1} />
+                Copy Address
+              </Button>
+
+              <Link
+                fontSize="sm"
+                display="flex"
+                alignItems="center"
+                href={`https://ropsten.etherscan.io/address/${account}`}
+                isExternal
+                color="gray.400"
+                ml={6}
+                _hover={{
+                  color: "whiteAlpha.800",
+                  textDecoration: "underline",
+                }}
+              >
+                <ExternalLinkIcon mr={1} />
+                View on Explorer
+              </Link>
+            </Flex>
+
           </Box>
         </ModalBody>
+
+        <ModalFooter
+          justifyContent="end"
+          background="gray.700"
+          borderBottomLeftRadius="3xl"
+          borderBottomRightRadius="3xl"
+          p={6}
+        >
+          <Text
+            color="white"
+            textAlign="left"
+            fontWeight="medium"
+            fontSize="md"
+          >
+            Your transactions will appear here ...
+          </Text>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   )
-
-
 }
