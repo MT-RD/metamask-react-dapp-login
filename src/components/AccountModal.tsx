0935e080404d4487c016b04c774b3e1584e63187
Manoj Talagadadeevi <manojt.854@gmail.com>
2021-08-12 10:08:08 -0400

    Added partial layout for the Account modal

--- src/components/AccountModal.tsx
+++ src/components/AccountModal.tsx
@@ -1,3 +1,67 @@
+import {useEthers} from "@usedapp/core";
+import {
+  Box,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text
+} from "@chakra-ui/react";
 
 
-export default function AccountModal() {}
+type Props = {
+  isOpen: any;
+  onClose: any;
+}
+
+// @ts-ignore
+export default function AccountModal({ isOpen, onClose }) {
+  const { account, deactivate } = useEthers();
+
+  return(
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="md">
+      <ModalOverlay />
+      <ModalContent
+        background="gray.900"
+        border="1px"
+        borderStyle="solid"
+        borderColor="gray.700"
+        borderRadius="3xl"
+      >
+        <ModalHeader color="white" px={4} fontSize="lg" fontWeight="medium">Account</ModalHeader>
+        <ModalCloseButton
+          color="white"
+          fontSize="sm"
+          _hover={{
+            color: "whiteAlpha.700"
+          }}
+        ></ModalCloseButton>
+
+        <ModalBody pt={0} px={4}>
+          <Box
+            borderRadius="3xl"
+            border="1px"
+            borderStyle="solid"
+            borderColor="gray.600"
+            px={5}
+            pt={4}
+            pb={2}
+            mb={3}
+          >
+            <Flex justifyContent="space-between" alignItems="center" mb={3}>
+              <Text color="gray.400" fontSize="sm">
+                Connected with MetaMask
+              </Text>
+            </Flex>
+
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+
+
+}
