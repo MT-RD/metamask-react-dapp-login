afe2d0ce3c19de4e257c9a3db8a9bc1d478b1eeb
Manoj Talagadadeevi <manojt.854@gmail.com>
2021-08-12 00:34:17 -0400

    Format & Style our Connect Button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,5 +1,6 @@
 import {useEtherBalance, useEthers} from "@usedapp/core";
 import {Box, Button, Text} from "@chakra-ui/react";
+import {formatEther} from "@ethersproject/units";
 
 function ConnectButton() {
   const {activateBrowserWallet, account} = useEthers();
@@ -13,7 +14,7 @@ function ConnectButton() {
     <Box>
       <Text color="white" fontSize="md">
         {/* etherBalance will be an object, so we stringify it */}
-        {etherBalance && JSON.stringify(etherBalance)} ETH
+        {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
       </Text>
     </Box>
   ): (
